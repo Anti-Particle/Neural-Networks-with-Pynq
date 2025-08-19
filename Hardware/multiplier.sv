232c7bb6ab44eb360252cdf238f5dfab71f4670b
Rishav <rishav.sen@gmail.com>
2025-08-19 13:18:17 -0400

    modified Neuron submodules

--- Hardware/multiplier.sv
+++ Hardware/multiplier.sv
@@ -10,19 +10,11 @@ module multiplier(
 
     assign fraction_prod = {1b'1, din_a[9:0]} * {1b'1, din_a[9:0]};
     assign mult_zero = (din_a[14:10] == 0 && din_a[9:0] == 0) | (din_b[14:10] == 0 && din_b[9:0] == 0) ? 1 : 0;
-
-
-    //Fraction Calculation
-    always_comb begin
-        
-    end   
-
-    //Exponent Calculation
-    always_comb begin
-        if (mult_zero) expo = 0;
-        else
-    end   
+    assign frac = (mult_zero) ? 0 : fraction_prod[21:12];
+    assign expo = (mult_zero) ? 0 : ((din_a[14:10] - 127) + (din_a[14:10] - 127) + 127);
 
     assign dout[15] = din_a[15] ^ din_b[15];
+    assign dout[14:10] = expo;
+    assign dout[9:0] = frac;
     
 endmodule
--- Hardware/summation.sv
+++ Hardware/summation.sv
@@ -1,67 +1,22 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/23/2025 04:35:04 PM
-// Design Name: 
-// Module Name: keys
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
-
-module keys(
-    input  logic [31:0]  keycode,
-    input  logic pxl_clk, 
-    output logic left,
-    output logic down,
-    output logic up,
-    output logic right
+module summation #(parameter Num_Inputs)(
+    input  logic [15:0] din [Num_Inputs],
+    output logic [15:0]  dout
     );
     
-    always_ff @ (posedge pxl_clk) begin
-        if (keycode[15:8] == 'h50 || keycode[7:0] == 'h50 || keycode[23:16] == 'h50 || keycode[31:24] == 'h50) begin
-            left = 1'b1;
-        end
-        else begin
-            left = 1'b0;
-        end
-    end
-    
-    always_ff @ (posedge pxl_clk) begin
-        if (keycode[15:8] == 'h51 || keycode[7:0] == 'h51 || keycode[23:16] == 'h51 || keycode[31:24] == 'h51) begin
-            down = 1'b1;
-        end
-        else begin
-            down = 1'b0;
-        end
-    end
-    
-    always_ff @ (posedge pxl_clk) begin
-        if (keycode[15:8] == 'h52 || keycode[7:0] == 'h52 || keycode[23:16] == 'h52 || keycode[31:24] == 'h52) begin
-            up = 1'b1;
-        end
-        else begin
-            up = 1'b0;
-        end
-    end
-    
-    always_ff @ (posedge pxl_clk) begin
-        if (keycode[15:8] == 'h4F || keycode[7:0] == 'h4F || keycode[23:16] == 'h4F || keycode[31:24] == 'h4F) begin
-            right = 1'b1;
-        end
-        else begin
-            right = 1'b0;
+    logic [4:0] expo;
+    logic [9:0] frac;
+    logic sign;
+
+    always_comb begin
+        expo = din[0][9:0];
+        frac = din[0][14:10];
+        sign = din[0][15];
+        for (int i = 1; i < Num_Inputs; i = i + 1) begin
+            if (din[i][14:10] > frac) begin
+                frac = din[i][14:10];
+            end
         end
     end
     

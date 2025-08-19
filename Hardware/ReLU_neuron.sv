47ec5a99fb47bbab328a9a3fa9b43e1e48a52232
Rishav <rishav.sen@gmail.com>
2025-08-19 13:48:54 -0400

    modified Neuron submodules

--- Hardware/ReLU_neuron.sv
+++ Hardware/ReLU_neuron.sv
@@ -1,68 +1,29 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/23/2025 04:35:04 PM
-// Design Name: 
-// Module Name: keys
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
-
-module keys(
-    input  logic [31:0]  keycode,
-    input  logic pxl_clk, 
-    output logic left,
-    output logic down,
-    output logic up,
-    output logic right
+module ReLU #(parameter PREV_LAYER = 5, parameter NEXT_LAYER = 4) (
+    input logic [15:0] weigted_inputs[PREV_LAYER],
+    input logic [15:0] bias,
+    input logic [15:0] weights[NEXT_LAYER]
+    output logic [15:0] weighted_outputs[NEXT_LAYER]
     );
     
-    always_ff @ (posedge pxl_clk) begin
-        if (keycode[15:8] == 'h50 || keycode[7:0] == 'h50 || keycode[23:16] == 'h50 || keycode[31:24] == 'h50) begin
-            left = 1'b1;
-        end
-        else begin
-            left = 1'b0;
-        end
-    end
-    
-    always_ff @ (posedge pxl_clk) begin
-        if (keycode[15:8] == 'h51 || keycode[7:0] == 'h51 || keycode[23:16] == 'h51 || keycode[31:24] == 'h51) begin
-            down = 1'b1;
-        end
-        else begin
-            down = 1'b0;
-        end
-    end
-    
-    always_ff @ (posedge pxl_clk) begin
-        if (keycode[15:8] == 'h52 || keycode[7:0] == 'h52 || keycode[23:16] == 'h52 || keycode[31:24] == 'h52) begin
-            up = 1'b1;
-        end
-        else begin
-            up = 1'b0;
-        end
-    end
-    
-    always_ff @ (posedge pxl_clk) begin
-        if (keycode[15:8] == 'h4F || keycode[7:0] == 'h4F || keycode[23:16] == 'h4F || keycode[31:24] == 'h4F) begin
-            right = 1'b1;
-        end
-        else begin
-            right = 1'b0;
-        end
-    end
-    
+    logic [15:0] weighted_sum, biased_sum, activation
+
+    summation sum (.Num_Inputs(PREV_LAYER)) (
+        .din(weighted_inputs),
+        .dout(weighted_sum)
+    );
+
+    summation bias_accounting (.Num_Inputs(2)) (
+        .din({weighted_sum, bias}),
+        .dout(biased_sum)
+    );
+
+    assign activation = (biased_sum[15]) ? 0 : biased_sum;
+
+    multiplier weighting[NEXT_LAYER] (
+        .din_a(activation),
+        .din_b(weights),
+        .dout(weighted_outputs)
+    );
 endmodule
